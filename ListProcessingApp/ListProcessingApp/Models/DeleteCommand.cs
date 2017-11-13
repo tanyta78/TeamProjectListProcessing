65c7c7ffe6581ca4d1e4565da202a04a920c7f9a
hrimar <hhristoff@yahoo.com>
2017-11-13 16:52:01 +0200

    Implement the 'delete' command. Fixes #8

--- ListProcessingApp/ListProcessingApp/Models/DeleteCommand.cs
+++ ListProcessingApp/ListProcessingApp/Models/DeleteCommand.cs
@@ -1,6 +1,10 @@
 ﻿namespace ListProcessingApp.Models
 {
+    using System;
     using System.Collections.Generic;
+    using System.IO;
+    using IO;
+  
 
     class DeleteCommand:Command
     {
@@ -9,8 +13,26 @@
         }
 
         public override void Execute()
-        {
-            throw new System.NotImplementedException();
+        {           
+            if (this.CmdData.Length != 2)
+            {
+                //to do - to change exception when created
+                throw new InvalidDataException();
+            }
+
+            var indexForDeleting = int.Parse(this.CmdData[1]);
+
+            if(indexForDeleting < 0 || indexForDeleting > Input.Count-1)
+            {
+                Console.WriteLine($"Error: invalid index {indexForDeleting}");
+            }
+            else
+            {
+                this.Input.RemoveAt(indexForDeleting);
+                OutputWriter.WriteMessageOnNewLine(string.Join(" ", this.Input));
+            }
+            
+            
         }
     }
 }
